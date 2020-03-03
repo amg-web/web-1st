9b6b1e4ed1d2a11ce28d4012c7d5979729650eb4
amg-web <a.gangan@gmail.com>
2020-03-03 19:32:17 +0200

    script update

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ git config --global user.email "a.gangan@gmail.com"
 git config --global user.name "a.gangan@gmail.com"
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ng.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
