5e0ff6744ea8ae4cb1564d46c613a70a240e1a02
amg-web <a.gangan@gmail.com>
2020-03-03 19:57:57 +0200

    gunicorn

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git config --global user.email "a.gangan@gmail.com"   
-git config --global user.name "a.gangan@gmail.com"
+git config --global user.name "anmg"
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ng.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ng.conf
+sudo ln -s /home/box/web/etc/hello.py etc/gunicorn.d/hello.py
 sudo unlink /etc/nginx/sites-enabled/default
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
