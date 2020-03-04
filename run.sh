7e48469185809ab70335aedb36c0c2c3c4d6df76
amg-web <a.gangan@gmail.com>
2020-03-04 17:10:02 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ git config --global user.email "a.gangan@gmail.com"
 git config --global user.name "anmg"
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ng.conf
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
-gunicorn -b 0.0.0.0:8080 -c ~/web/etc/hello.py hello:app
+gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app
 sudo /etc/init.d/nginx restart
 
 sudo cat /var/log/nginx/error.log
