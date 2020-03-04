5ef00331a2563f03843642db3959418b62b14b7a
amg-web <a.gangan@gmail.com>
2020-03-04 17:31:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ng.conf
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
-gunicorn -D -b 0.0.0.0:8080 -c ~/web/hello.py hello:app
+
 sudo /etc/init.d/nginx restart
 
 sudo cat /var/log/nginx/error.log
+gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app
