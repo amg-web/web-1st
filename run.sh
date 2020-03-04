7ebc8b3d19f8bfd9b792e007a460bf143b13bdff
amg-web <a.gangan@gmail.com>
2020-03-04 11:31:27 +0200

    corr

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@ git config --global user.email "a.gangan@gmail.com"
 git config --global user.name "anmg"
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ng.conf
-sudo ln -s /home/box/web/etc/hello.py etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
-gunicorn -D hello:app --bind='0.0.0.0:8080'
+gunicorn hello:app --bind='0.0.0.0:8080'
 sudo /etc/init.d/nginx restart
 
+sudo cat /var/log/nginx/error.log
