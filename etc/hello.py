55e489e1ab1a48d939538be10e001f8cd7821463
amg-web <a.gangan@gmail.com>
2020-03-04 17:04:30 +0200

    proxy

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,12 @@
 def app(environ, start_response):
 
-    data = environ.split('&')
+    data = environ.replace('&', '\n')
+
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+
     ]
     start_response(status, response_headers)
-    return iter(data)
+    return data
 
