6dc09c8a2892912fd5cc5b502739b4920460267d
amg-web <a.gangan@gmail.com>
2020-03-04 17:29:31 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
 
     body = environ['QUERY_STRING']
-
+    print(body)
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
