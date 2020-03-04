2a889e9f46b8dba798c5e438c228943f55742ef8
amg-web <a.gangan@gmail.com>
2020-03-04 17:15:14 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
 def app(environ, start_response):
 
-    data = environ.replace('&', '\n')
+    body = environ['QUERY_STRING']
 
     status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-
-    ]
+    response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return data
+    return body
 
